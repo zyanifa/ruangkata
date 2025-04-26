4c810acd924aa18c9227209d92f571b7b121bf15
beng <howlingdawn7@gmail.com>
2025-04-26 17:17:59 +0700

    migration fixed

--- database/migrations/2025_03_01_105043_create_posts_table.php
+++ database/migrations/2025_03_01_105043_create_posts_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('image')->nullable();
             $table->string('title');
             $table->string('slug')->unique();
+            $table->text('short_description')->nullable();
             $table->longText('content');
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
