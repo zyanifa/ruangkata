6b311511858ec998b8d46fd93fccc7973152d123
beng <howlingdawn7@gmail.com>
2025-04-29 14:58:09 +0700

    small fix

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Lupa kata sandi? Tidak masalah. Silakan isi alamat email Anda, dan kami akan kirim tautan untuk reset kata sandi. Melalui tautan tersebut, Anda dapat membuat kata sandi baru.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Kirim Tautan Reset') }}
             </x-primary-button>
         </div>
     </form>
