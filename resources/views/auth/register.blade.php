482c1df68de79dd71698eca5021f41baf24b9d80
zyanifa <howlingdawn7@gmail.com>
2025-05-09 11:23:24 +0700

    small fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -25,7 +25,7 @@
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Kata sandi')" />
+            <x-input-label for="password" :value="__('Kata Sandi')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
